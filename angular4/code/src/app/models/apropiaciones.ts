77a0077ab58081e9bab94b5cb2d7366b7bd3c861
jcamilosarmientor <jcamilosarmientor@gmail.com>
2018-05-10 10:50:33 -0500

    Removiendo cosas innecesarias

--- angular4/code/src/app/models/apropiaciones.ts
+++
@@ -1,9 +0,0 @@
-
-import { Movimiento } from './movimiento';
-
-export class Apropiaciones {
-   _id: string;
-  vigencia:	int;
-  valor_inicial:	float64;
-  movimientos: Movimiento[];
-}
--- angular4/code/src/app/models/disponibilidadapropiacion.ts
+++
@@ -1,9 +0,0 @@
-
-import { Apropiacion } from './apropiacion';
-
-export class Disponibilidadapropiacion {
-   _id: string;
-  valor:	int;
-  fuente_financiamiento:	int;
-  apropiacion: Apropiacion[];
-}
